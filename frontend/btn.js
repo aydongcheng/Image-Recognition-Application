231e5a79c33784edfcc543dd35abf614fbebb7ea
王媛原 <1299795278@qq.com>
2022-11-28 22:53:51 -0500

    frontend

--- frontend/btn.js
+++ frontend/btn.js
@@ -2,16 +2,43 @@
 //      let ub = document.getElementsById('btn').click();
 //      alert(ub);
 // }
+
 window.onload = function(){
-     const img = document.querySelector("#uploadFile");
+     var img = document.querySelector("#uploadFile");
      img.addEventListener("change", function(){
-          const reader = new FileReader();
+          var reader = new FileReader();
           reader.addEventListener("load", () => {
-               const uploadImg = reader.result;
+               var uploadImg = reader.result;
                document.querySelector("#displayImg").style.backgroundImage = `url(${uploadImg})`;
           });
           reader.readAsDataURL(this.files[0]);
      })
 }
+function sendImg(){
+     var file = document.getElementById("uploadFile").files[0];
+     var formData = new FormData();
+     formData.append("inFile", file);
+     var xhr = new XMLHttpRequest();
+     xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+               var res = "This image is mostly likely " + JSON.parse(xhr.responseText)['result'];
+               var resdiv = document.getElementById("result");
+               resdiv.innerHTML = res
+          }
+     }
+     xhr.open("POST", "http://192.168.0.254:8000/img");
+     xhr.send(formData);
+}
+
+
+// function sendImg() {
+//      const obj = {inFile: img};
+//      const url = "http://127.0.0.1:8000/img";
+//      $.post(url, obj, function(data, status) {
+//           console.log(`${data} and status is ${status}`);
+//      })
+//      ;
+// }
+
 
 
