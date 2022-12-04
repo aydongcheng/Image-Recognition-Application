26a85183087e5d2ed62a68c9b54c53d00b845875
dongzc11 <dongzc11@node-0.image-recognition.ch-geni-net.instageni.washington.edu>
2022-12-04 13:38:26 -0800

    deploy fix

--- app.py
+++ app.py
@@ -24,3 +24,6 @@ async def uploadFile(inFile: UploadFile = File(...)):
     else:
         return {"result": "Not a valid image"}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=25000)
