350348d7caa8ce7a3b5916bc25e626b2c025477a
Lightbringer <lighter0610@gmail.com>
2022-12-08 18:08:30 -0500

    remove can't use

--- backend/backend.sh
+++ backend/backend.sh
@@ -2,6 +2,4 @@ sudo apt-get install python3-pip
 sudo apt-get install libjpeg-dev zlib1g-dev
 pip3 install fastapi
 pip3 install aiofiles
-pip3 install torch==1.10.1+cpu torchvision==0.11.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install uvicorn==0.16.0
-python3 app.py
