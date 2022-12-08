44d082a6277bc56382c0f64b19fc344824e76161
Lightbringer <lighter0610@gmail.com>
2022-12-08 17:31:47 -0500

    create sh scripts

---
+++ frontend/frontend.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo apt-get install -y curl
+sudo cp -R /local/frontend/* /var/www/
+sudo rm -rf /var/www/html
+sudo ln -s /var/www/ /var/www/html
+
+sudo /etc/init.d/apache2 force-reload
+sudo service apache2 restart
